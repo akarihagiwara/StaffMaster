e093a317f30339723e13a35eb778580fa5382f8c
akarihagiwara <akari.hagiwara3ef@gmail.com>
2026-09-14 17:54:35 +0900

    ver1.01

--- backend/src/main/resources/data-postgres.sql
+++ backend/src/main/resources/data-postgres.sql
@@ -18,17 +18,15 @@ UPDATE
 SET
     position_name = EXCLUDED.position_name;
 
-INSERT INTO
-    staffs (
-        staff_id,
-        staff_name,
-        department_id,
-        position_id,
-        email,
-        login_id,
-        login_password
-    )
-VALUES
+INSERT INTO staffs (
+    staff_id,
+    staff_name,
+    department_id,
+    position_id,
+    email,
+    login_id,
+    login_password
+) VALUES
     (
         'S001',
         '管理 太郎',
@@ -55,34 +53,5 @@ VALUES
         'jiro@example.com',
         'dev0001',
         'dev1234'
-    ) ON CONFLICT (staff_id) DO NOTHING;
-
-(
-    'S001',
-    '管理 太郎',
-    'D001',
-    'P001',
-    'admin@example.com',
-    'admin01',
-    'pass123'
-),
-(
-    'S002',
-    '営業 花子',
-    'D002',
-    'P002',
-    'hanako@example.com',
-    'sales01',
-    'sales123'
-),
-(
-    'S003',
-    '開発 次郎',
-    'D003',
-    'P003',
-    'jiro@example.com',
-    'dev0001',
-    'dev1234'
-);
-
+    )
 ON CONFLICT (staff_id) DO NOTHING;
